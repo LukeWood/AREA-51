8a3f6e29866c50dee83ffe019d3edb8999e30806
LukeWood <lswood@smu.edu>
2016-10-12 18:28:38 -0500

    minor tuning

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var map = new UFOs(document,"data/data.json");
-var years = {}
+var years = {};
 var selector;
 $.getJSON("ufos.json",function(data){
   selector = document.createElement("select");
@@ -7,25 +7,48 @@ $.getJSON("ufos.json",function(data){
   selector.style.top=0;
   selector.style.left=0;
   $(selector).change(function(){
+    map.clear();
       var year = selector.selectedIndex;
       year = selector.options[year].value;
 
       var data = years[year];
-      if(data != undefined){
-        var x = data[0];
-        var y=  data[1];
-        x = (x+124.3)/(48.5+124.3);
-        y = (y+85)/(85+24.8);
-        console.log(x,y);
-        map.addUFOPercent(x,y);
+      for(var i = 0; i < data.length; i++)
+      {
+        tdata = data[i];
+        if(tdata != undefined){
+          var x = tdata[0];
+          var y=  tdata[1];
+          var xi = x, yi = y;
+          //console.log(x);
+          y +=81.5;
+          y/=-124.92+81.5;
+          x-=26.45;
+          x/=60.55-28.5;
+          if(x < 1 && x > 0 && y < 1 && y > 0){
+          //console.log(x)
+            map.addUFOPercent(x,y);
+          }
+          else{
+              if(x < 0){
+                console.log("X < 1 : ",xi);
+              }
+          }
+        }
       }
+
   });
     data.forEach(function(ufo){
-        years[ufo[0].toString()] = [ufo[2],ufo[3]];
-        var date = document.createElement("option");
-        date.setAttribute("value", ufo[0].toString());
-        date.innerHTML = ufo[0].toString();
-        selector.appendChild(date);
+      var year = ufo[0].substring(0,6);
+        if(year in years){
+            years[year].push([ufo[2],ufo[3]]);
+        }else{
+          years[year] = [[ufo[2],ufo[3]]];
+          var date = document.createElement("option");
+          date.setAttribute("value", year);
+          date.innerHTML = year;
+          selector.appendChild(date);
+        }
+
     })
     document.body.appendChild(selector);
 });
