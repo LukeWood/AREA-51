aedb95ef72243cbdf88288af17a9e844aa9668c0
LukeWood <lswood@smu.edu>
2016-10-13 13:44:41 -0500

    another update

--- main.js
+++ main.js
@@ -9,37 +9,45 @@ selector.style.top=0;
 selector.style.left=0;
 document.body.appendChild(selector);
 
+var index = -1;
+
 $.getJSON("used_data/years.json",function(data){
     years = data;
-    year = years[0];
-    selector.innerHTML = year;
-    $.getJSON("used_data/"+year+".json",function(data){
-        map.destroy();
-        map = new UFOs(document);
-        var tdata;
-        for(var i = 0; i < data.length; i++)
-        {
-              tdata = data[i];
-              if(tdata != undefined){
-                var x = tdata[2];
-                var y=  tdata[3];
-                var xi = x, yi = y;
-                //console.log(x);
-                y +=81.5;
-                y/=-145.68+81.5;
-                x-=26.45;
-                x/=60.55-28.5;
-                if(x < 1 && x > 0 && y < 1 && y > 0){
-                  map.addUFOPercent(x,y);
-            }
+    setInterval(update,3000);
+});
+function update(){
+  index++;
+  if(index > years.length -1){
+    index = 0;
+  }
+  year = years[index];
+  selector.innerHTML = year.substring(0,4) + "/"+year.slice(-2);
+
+  $.getJSON("used_data/"+year+".json",function(data){
+      map.destroy();
+      map = new UFOs(document);
+      var tdata;
+      for(var i = 0; i < data.length; i++)
+      {
+            tdata = data[i];
+            if(tdata != undefined){
+              var x = tdata[2];
+              var y=  tdata[3];
+              var xi = x, yi = y;
+              //console.log(x);
+              y +=81.5;
+              y/=-145.68+81.5;
+              x-=26.45;
+              x/=60.55-28.5;
+              if(x < 1 && x > 0 && y < 1 && y > 0){
+                map.addUFOPercent(x,y);
           }
         }
+      }
+      map.startAnimation();
 
-            map.startAnimation();
-
-    });
-});
-
+  });
+}
 $(window).resize(function(){
   map.resize();
 });
