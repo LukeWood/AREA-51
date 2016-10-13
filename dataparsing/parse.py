259f4262ae29f1a058fd2934c2e6a8f08fd48cab
LukeWood <lswood@smu.edu>
2016-10-13 13:17:07 -0500

    created parser

--- dataparsing/parse.py
+++ dataparsing/parse.py
@@ -8,31 +8,36 @@ all_spottings = []
 
 for line in open("../data/ufos.tsv"):
     split = line.split('\t')
-    all_spottings.append([split[0],split[2]])
+    date2 = 0
+    try:
+        date2 = int(split[1])
+    except:
+        date2=0
+    date = str(split[0]) if int(split[0]) > date2 else str(date2)
+    all_spottings.append([date,split[2]])
 
-out_file = "0000"
-of = None
+out_file = "abcdefg"
+of = False
 all_spottings.sort(key=lambda x: x[0])
 to_print = []
 
-for spot in all_spottings:
+for spot in all_spottings[:1000]:
+    if(spot[0][:4] == "0000"):
+        continue
     try:
         location = geolocator.geocode(spot[1])
     except:
         continue
-    print(spot[0])
     if not (out_file == str(spot[0])[:4]):
-        print("now setting file to: out/"+str(spot[0])[:4]+".json")
-        if of:
+        if of != False:
+            to_print.sort(key=lambda x: x[0])
             of.write(json.dumps(to_print))
             to_print = []
             of.close()
+        print("OPENING "+ str(spot[0])[:4])
         of = open("out/"+str(spot[0])[:4]+".json","w")
         out_file = str(spot[0])[:4]
-    else:
-        print(out_file + " equals "+ str(spot[0])[:4])
     if location is not None:
         to_print.append([spot[0],spot[1],location.latitude,location.longitude])
-
-to_print.sort(key=lambda x: x[0])
-print(json.dumps(to_print))
+    else:
+        print("location was none for "+str(spot[1]))
